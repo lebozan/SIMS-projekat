5ee12fbf9322984b780f9cabe66c021f38e0ea75
Bojan Cakic <cakicbojan@gmail.com>
2018-07-16 07:53:41 +0200

    prozor sa dugmicima za svakog korisnika i funkcionalan login

--- NaplatneRampe/src/gui/Aplikacija.java
+++ NaplatneRampe/src/gui/Aplikacija.java
@@ -1,20 +1,24 @@
 package gui;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.System;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import korisnici.Administrator;
 import korisnici.Korisnik;
+import korisnici.Operater;
+import korisnici.SefCentrale;
+import korisnici.SefStanice;
 
 public class Aplikacija extends Application{
 	
-	Button b;
+	Stage prozor;
+	GridPane sadrzaj;
 	LogInProzor login;
 	Korisnik trenutniKorisnik;
 	
@@ -25,18 +29,150 @@ public class Aplikacija extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		prozor = primaryStage;
+		prozor.setTitle("Aplikacija");
+		sadrzaj = new GridPane();
 		
-		primaryStage.setTitle("Aplikacija");
-		b = new Button();
-		b.setText("klikni me pls");
+		Scene okvir = new Scene(sadrzaj, 600, 500);
 		login = new LogInProzor();
-		StackPane layout = new StackPane();
-		layout.getChildren().add(b);
-		Scene s = new Scene(layout, 1000, 600);
-		primaryStage.setScene(s);
-		primaryStage.show();
-		login.toFront();
+		prozor.setScene(okvir);
+		prozor.show();
 		
+		login.setRoditelj(this);
+		login.setAlwaysOnTop(true);
+		prozor.setOnCloseRequest(e -> Platform.exit());
+		
+	}
+	
+	public void postaviTrenutnogKorisnika(String uname,String pword, String ime, String prezime, int uloga) {
+		switch (uloga) {
+			case 0: trenutniKorisnik = new Administrator(uname, pword, ime, prezime);
+					adminMeni(sadrzaj);
+					break;
+			case 1: trenutniKorisnik = new SefCentrale(uname, pword, ime, prezime);
+					sefCentraleMeni(sadrzaj);
+					break;
+			case 2: trenutniKorisnik = new SefStanice(uname, pword, ime, prezime);
+					sefStaniceMeni(sadrzaj);
+					break;
+			case 3: trenutniKorisnik = new Operater(uname, pword, ime, prezime);
+					operatorStaniceMeni(sadrzaj);
+					break;	
+		}
+		
+	}
+	
+	private void adminMeni(GridPane layout) {
+		Button b1 = new Button("Unos korisnika");
+		b1.setPrefSize(110, 110);
+		Button b2 = new Button("Unos centrale");
+		b2.setPrefSize(110, 110);
+		Button b3 = new Button("Unos stanice");
+		b3.setPrefSize(110, 110);
+		Button b4 = new Button("Izmena korisnika");
+		b4.setPrefSize(110, 110);
+		Button b5 = new Button("Izmena centrale");
+		b5.setPrefSize(110, 110);
+		Button b6 = new Button("Izmena stanice");
+		b6.setPrefSize(110, 110);
+		
+		layout.setAlignment(Pos.CENTER);
+		layout.setHgap(30);
+		layout.setVgap(20);
+		layout.add(b1, 0, 1);
+		layout.add(b2, 0, 2);
+		layout.add(b3, 0, 3);
+		layout.add(b4, 1, 1);
+		layout.add(b5, 1, 2);
+		layout.add(b6, 1, 3);
+	}
+	
+	private void sefCentraleMeni(GridPane layout) {
+		Button b1 = new Button("Izvestaj broja vozila stanice za datum");
+		b1.setWrapText(true);
+		b1.setPrefSize(110, 110);
+		Button b2 = new Button("Izvestaj broja vozila stanice za period");
+		b2.setWrapText(true);
+		b2.setPrefSize(110, 110);
+		Button b3 = new Button("Izvestaj ukupne zarade stanice za datum");
+		b3.setWrapText(true);
+		b3.setPrefSize(110, 110);
+		Button b4 = new Button("Izvestaj ukupne zarade stanice za period");
+		b4.setWrapText(true);
+		b4.setPrefSize(110, 110);
+		Button b5 = new Button("Izvestaj broja vozila centrale za datum");
+		b5.setWrapText(true);
+		b5.setPrefSize(110, 110);
+		Button b6 = new Button("Izvestaj broja vozila centrale za period");
+		b6.setWrapText(true);
+		b6.setPrefSize(110, 110);
+		Button b7 = new Button("Izvestaj ukupne zarade centrale za datum");
+		b7.setWrapText(true);
+		b7.setPrefSize(110, 110);
+		Button b8 = new Button("Izvestaj ukupne zarade centrale za period");
+		b8.setWrapText(true);
+		b8.setPrefSize(110, 110);
+		
+		layout.setAlignment(Pos.CENTER);
+		layout.setHgap(30);
+		layout.setVgap(20);
+		layout.add(new Label("Izvestaji stanice"), 0, 0);
+		layout.add(new Label("Izvestaji centrale"), 1, 0);
+		layout.add(b1, 0, 1);
+		layout.add(b2, 0, 2);
+		layout.add(b3, 0, 3);
+		layout.add(b4, 0, 4);
+		layout.add(b5, 1, 1);
+		layout.add(b6, 1, 2);
+		layout.add(b7, 1, 3);
+		layout.add(b8, 1, 4);
+	}
+	
+	private void sefStaniceMeni(GridPane layout) {
+		Button b1 = new Button("Izvestaj broja vozila stanice za datum");
+		b1.setWrapText(true);
+		b1.setPrefSize(110, 110);
+		Button b2 = new Button("Izvestaj broja vozila stanice za period");
+		b2.setWrapText(true);
+		b2.setPrefSize(110, 110);
+		Button b3 = new Button("Izvestaj ukupne zarade stanice za datum");
+		b3.setWrapText(true);
+		b3.setPrefSize(110, 110);
+		Button b4 = new Button("Izvestaj ukupne zarade stanice za period");
+		b4.setWrapText(true);
+		b4.setPrefSize(110, 110);
+		Button b5 = new Button("Promena cene deonice");
+		b5.setWrapText(true);
+		b5.setPrefSize(110, 110);
+		
+		layout.setAlignment(Pos.CENTER);
+		layout.setHgap(30);
+		layout.setVgap(20);
+		layout.add(b1, 0, 1);
+		layout.add(b2, 0, 2);
+		layout.add(b3, 1, 1);
+		layout.add(b4, 1, 2);
+		layout.add(b5, 2, 1);
+
+	}
+	
+	private void operatorStaniceMeni(GridPane layout) {
+		Button b1 = new Button("Naplata putarine");
+		b1.setWrapText(true);
+		b1.setPrefSize(110, 110);
+		Button b2 = new Button("Dizanje rampe");
+		b2.setWrapText(true);
+		b2.setPrefSize(110, 110);
+		Button b3 = new Button("Prijava greske");
+		b3.setWrapText(true);
+		b3.setPrefSize(110, 110);
+		
+		layout.setAlignment(Pos.CENTER);
+		layout.setHgap(30);
+		layout.setVgap(20);
+		layout.add(b1, 0, 1);
+		layout.add(b2, 1, 1);
+		layout.add(b3, 2, 1);
 	}
 	
 }
--- NaplatneRampe/src/gui/LogInProzor.java
+++ NaplatneRampe/src/gui/LogInProzor.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -23,16 +24,17 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import korisnici.Administrator;
 import korisnici.Korisnik;
-import korisnici.Operater;
-import korisnici.SefCentrale;
-import korisnici.SefStanice;
-import korisnici.UlogaKorisnika;
 
-public class LogInProzor extends Stage {
+public class LogInProzor extends Stage implements EventHandler<ActionEvent>{
 	
-	private Korisnik ulogovan;
+	private Aplikacija roditelj;
+	public boolean uspesanLogin;
+	private Button btnSignIn;
+	private Button btnExit;
+	private TextField unField;
+	private PasswordField pwField;
+	private final Text loginMsg = new Text();
 
 	public LogInProzor() {
 		super();
@@ -51,44 +53,36 @@ public class LogInProzor extends Stage {
         Label userName = new Label("User Name:");
         grid.add(userName, 0, 1);
 
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
+        unField = new TextField();
+        grid.add(unField, 1, 1);
 
         Label pw = new Label("Password:");
         grid.add(pw, 0, 2);
 
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+        pwField = new PasswordField();
+        grid.add(pwField, 1, 2);
         
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
-
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
-//        boolean uspesanLogin;
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent e) {
-            	boolean potvrda = proveriLogin(userTextField.getText(), pwBox.getText());
-            	
-            	if (potvrda) {
-//            		uspesanLogin = potvrda;
-            		actiontarget.setFill(Color.LIMEGREEN);
-            		actiontarget.setText("Uspesno ste se ulogovali!");
-            	} else {
-            		actiontarget.setFill(Color.FIREBRICK);
-            		actiontarget.setText("Neuspesan login!\nProbajte ponovo");
-            	}
-                                
-            }
-        });
+        btnSignIn = new Button("Sign in");
+        btnSignIn.setOnAction(this::handle);
+        HBox hb = new HBox(10);
+        hb.setAlignment(Pos.BOTTOM_RIGHT);
+        hb.getChildren().add(btnSignIn);
+        btnExit = new Button("Exit");
+        btnExit.setOnAction(this::handle);
+        hb.getChildren().add(btnExit);
+        grid.add(hb, 1, 4);
+        
+        grid.add(loginMsg, 1, 6);
+        
+//        btn.setOnAction(e -> {
+//        	
+//        });
+        
+//        this.setOnCloseRequest(e -> Platform.exit());
 
         Scene scene = new Scene(grid, 300, 275);
         this.setScene(scene);
+//        this.initModality(Modality.APPLICATION_MODAL);
         this.show();
 	}
 
@@ -97,19 +91,6 @@ public class LogInProzor extends Stage {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void postaviTrenutnogKorisnika(String uname,String pword, String ime, String prezime, int uloga) {
-		switch (uloga) {
-			case 0: ulogovan = new Administrator(uname, pword, ime, prezime);
-					break;
-			case 1: ulogovan = new SefCentrale(uname, pword, ime, prezime);
-					break;
-			case 2: ulogovan = new SefStanice(uname, pword, ime, prezime);
-					break;
-			case 3: ulogovan = new Operater(uname, pword, ime, prezime);
-					break;
-				
-		}
-	}
 	
 	private boolean proveriLogin(String un, String pw) {
 		
@@ -125,7 +106,7 @@ public class LogInProzor extends Stage {
 				String[] tokens = line.split("[|]");
 				if (tokens[0].equals(un) && tokens[1].equals(pw)) {
 					retval = true;
-					postaviTrenutnogKorisnika(tokens[0], tokens[1], tokens[2], tokens[3], Integer.parseInt(tokens[4]));
+					roditelj.postaviTrenutnogKorisnika(tokens[0], tokens[1], tokens[2], tokens[3], Integer.parseInt(tokens[4]));
 					break;
 				}
 			}
@@ -137,15 +118,43 @@ public class LogInProzor extends Stage {
 		}
 		return retval;
 	}
-
-	public Korisnik getUlogovan() {
-		return ulogovan;
-	}
-
-	public void setUlogovan(Korisnik ulogovan) {
-		this.ulogovan = ulogovan;
-	}
 	
+	private void zatvori() {
+		
+		this.close();
+	}
+
+	public Aplikacija getRoditelj() {
+		return roditelj;
+	}
+
+	public void setRoditelj(Aplikacija roditelj) {
+		this.roditelj = roditelj;
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		if (event.getSource().equals(btnSignIn)) {
+			boolean potvrda = proveriLogin(unField.getText(), pwField.getText());
+	    	if (potvrda) {
+	    		loginMsg.setFill(Color.LIMEGREEN);
+	    		loginMsg.setText("Uspesno ste se ulogovali!");
+	    		
+//	    		uspesanLogin = true;
+//	    		AlertProzor.display("Uspesan login", "Ulogovani ste kao korisnik " + userTextField.getText());
+	    	} else {
+	    		loginMsg.setFill(Color.FIREBRICK);
+	    		loginMsg.setText("Neuspesan login!\nProbajte ponovo");
+//	    		uspesanLogin = false;
+//	    		AlertProzor.display("Neuspesan login", "Probajte ponovo");
+	    	}
+//	    	zatvori();
+		} else if (event.getSource().equals(btnExit)) {
+			Platform.exit();
+			this.close();
+		}
+		
+	}
 	
 
 }
